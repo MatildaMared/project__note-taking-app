6937ba2e1ec63082dd02b1d63e11647af45bb2ec
Matilda Mared <matildamared@live.se>
2024-11-20 20:48:26 +0100

    add styled components

---
+++ app/components/Menu/Menu.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import styled from "styled-components";
+
+function Menu() {
+    return <Wrapper>Menu</Wrapper>;
+}
+
+const Wrapper = styled.div`
+    border: 1px solid hotpink;
+`;
+
+export default Menu;
