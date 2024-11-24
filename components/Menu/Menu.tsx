66fe41ffa66a905be9e50e2b20ff9386f1bcc92d
Matilda Mared <matildamared@live.se>
2024-11-24 21:47:07 +0100

    add support to toggle dark mode

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -1,11 +1,24 @@
 "use client";
+import Image from "next/image";
+import useTheme from "@/hooks/useTheme";
 
 import React from "react";
 
 function Menu() {
+    const { theme, toggleDarkMode } = useTheme();
+
+    const logoImg = theme === "dark" ? "/images/logo-dark.svg" : "/images/logo.svg";
+
     return (
-        <menu className="h-full min-w-60 border-r border-r-neutral-200 p-4 dark:border-r-neutral-700">
+        <menu className="flex h-full min-w-60 flex-col border-r border-r-neutral-200 p-4 dark:border-r-neutral-700">
+            <Image src={logoImg} alt="Logo" width={100} height={95} />
             <p>Menu</p>
+            <button
+                onClick={toggleDarkMode}
+                className="mt-auto cursor-pointer rounded-md bg-neutral-300 px-2 py-1 text-neutral-500 dark:bg-neutral-800"
+            >
+                {theme === "dark" ? "Switch to Light Mode" : "Switch to Dark Mode"}
+            </button>
         </menu>
     );
 }
---
+++ hooks/useTheme.tsx
@@ -0,0 +1,46 @@
+// create a hook that returns the current theme and a function to toggle it
+
+import React from "react";
+
+export type Theme = "light" | "dark";
+
+export default function useDarkMode() {
+    const [theme, setTheme] = React.useState("light");
+
+    React.useEffect(() => {
+        const themeFromStorage = window.localStorage.getItem("theme");
+        if (themeFromStorage && isValidTheme(themeFromStorage)) {
+            setTheme(themeFromStorage);
+            return;
+        } else {
+            if (typeof window !== "undefined") {
+                const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+                if (!themeFromStorage) {
+                    setTheme(prefersDark ? "dark" : "light");
+                }
+            }
+        }
+    }, []);
+
+    React.useEffect(() => {
+        if (theme === "dark") {
+            document.body.classList.add("dark");
+        } else {
+            document.body.classList.remove("dark");
+        }
+    }, [theme]);
+
+    function toggleDarkMode() {
+        setTheme((currentTheme) => {
+            const newTheme = currentTheme === "light" ? "dark" : "light";
+            window.localStorage.setItem("theme", newTheme);
+            return newTheme;
+        });
+    }
+
+    return { theme, toggleDarkMode };
+}
+
+function isValidTheme(theme: string): theme is Theme {
+    return theme === "light" || theme === "dark";
+}
